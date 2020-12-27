cbd7c2b776e7110d7981dcb2a4bcb6b2ce762e08
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2020-12-27 12:07:54 +0900

    commit

---
+++ commnad_line.java
@@ -0,0 +1,57 @@
+<body>
+    <div class="container">
+        <table width="100%" height="100%" class="bg-lime padding-tb">
+            <tr width="100%" height="100%">
+                <td width="100%" height="100%">
+                    <table width="75%" height="90%" class="block-c text-top">
+                        <tr width="100%" height="15%">
+                            <td class="font-white text-c bg-blue">
+                                <p>Command Line Echo</p>
+                            </td>
+                        </tr>
+                        <tr width="100%" height="75%">
+                            <td height="100%" class="font-white text-l text-top bg-black">
+                                <div class="box scrolly" id="command-history">
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                    <span class="padding-lr">a</br></span>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr width="100%" height="10%">
+                            <td width="90%" class="bg-black">
+                                <span class="padding-lr">
+                                    <input type="text" name="command" placeholder="type any command" style="width: 95%;" id="command"></input>
+                                </span>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </div>
+</body>
