3116c2f1d6aacae407dcacb709f9966e53575b44
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2020-12-27 14:03:57 +0900

    comiit

--- command_line.js
+++ command_line.js
@@ -1,3 +1,33 @@
+class Stack{
+    constructor() {
+        this.arr = [];
+    }
+    // 末尾に要素を挿入
+    push(ele){
+        this.arr.push(ele);
+    }
+    // 末尾の要素を取得及び配列から削除
+    pop(){
+        if(this.isEmpty()) return "";
+        return this.arr.pop();
+    }
+    // 末尾の要素を取得、配列からは削除されない
+    peek(){
+        return this.arr[this.arr.length-1];
+    }
+    // スタックが空かどうかを判断
+    isEmpty(){
+        return this.arr.length <= 0 ? true : false;
+    }
+}
+
+// 現在の入力欄のコマンド以前の履歴
+let beforeHistory = new Stack();
+// 現在の入力欄のコマンド以降の履歴
+let afterHistory = new Stack();
+
+
+// 入力欄でのイベント定義(Enter, 上下の矢印)
 document.getElementById("command").addEventListener("keydown", event => {
     let ele = document.getElementById("command");
     if(event.keyCode == 13){
@@ -6,16 +36,27 @@ document.getElementById("command").addEventListener("keydown", event => {
         let atmark = createSpanTag(" @ ", ["font-pink"]);
         let recursionist = createSpanTag("recursionist", ["font-blue"]);
         let command = createSpanTag(`: ${ele.value}</br>`, []);
+        while(!afterHistory.isEmpty()){
+            beforeHistory.push(afterHistory.pop());
+        }
+        beforeHistory.push(ele.value);
+        console.log(beforeHistory.arr);
         ele.value = "";
         commandHistory.append(student, atmark, recursionist, command);
     }else if(event.keyCode == 38){
-        console.log("arrowap")
+        if(beforeHistory.isEmpty()) return;
+        if(ele.value.length > 0) afterHistory.push(ele.value);
+        let value = beforeHistory.pop();
+        ele.value = value;
     }else if(event.keyCode == 40){
-        console.log("arrowdown")
+        if(afterHistory.isEmpty()) return;
+        if(ele.value.length > 0) beforeHistory.push(ele.value);
+        ele.value = afterHistory.pop();
     }else{
     }
 });
 
+// spanタグの要素を作成
 let createSpanTag = (innerHTML, classNameArr) => {
     let span = document.createElement("span");
     span.innerHTML = innerHTML;
@@ -24,3 +65,4 @@ let createSpanTag = (innerHTML, classNameArr) => {
     }
     return span;
 }
+
