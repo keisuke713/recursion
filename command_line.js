2ddd0fa1f8465eda452cc11f111c79279cec9668
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2020-12-27 12:22:27 +0900

    メソッド作成

--- command_line.js
+++ command_line.js
@@ -2,26 +2,25 @@ document.getElementById("command").addEventListener("keydown", event => {
     let ele = document.getElementById("command");
     if(event.keyCode == 13){
         let commandHistory = document.getElementById("command-history");
-        let student = document.createElement("span");
-        let atmark = document.createElement("span");
-        let recursionist = document.createElement("span");
-        let command = document.createElement("span");
-        student.innerHTML = "student ";
-        student.classList.add("font-lime");
-        atmark.innerHTML = "@";
-        atmark.classList.add("font-pink");
-        recursionist.innerHTML = " recursionist";
-        recursionist.classList.add("font-blue");
-        command.innerHTML = `:${ele.value}</br>`
+        let student = createSpanTag("student", ["font-lime"]);
+        let atmark = createSpanTag(" @ ", ["font-pink"]);
+        let recursionist = createSpanTag("recursionist", ["font-blue"]);
+        let command = createSpanTag(`: ${ele.value}</br>`, []);
         ele.value = "";
-        commandHistory.append(student);
-        commandHistory.append(atmark);
-        commandHistory.append(recursionist);
-        commandHistory.append(command);
+        commandHistory.append(student, atmark, recursionist, command);
     }else if(event.keyCode == 38){
         console.log("arrowap")
     }else if(event.keyCode == 40){
         console.log("arrowdown")
     }else{
     }
-})
+});
+
+let createSpanTag = (innerHTML, classNameArr) => {
+    let span = document.createElement("span");
+    span.innerHTML = innerHTML;
+    for(i in classNameArr){
+        span.classList.add(classNameArr[i]);
+    }
+    return span;
+}
