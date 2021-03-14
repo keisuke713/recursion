6cfcfdbdcaf49a94f6746ae1c9a2e79946e73e7b
根橋京輔 <nebashikeisuke@nebashimiyakotasukunoMacBook-Air.local>
2021-03-14 20:03:38 +0900

    右に行くときと左に行く時で画像の向きを帰る

--- run_melos/run_melos.js
+++ run_melos/run_melos.js
@@ -1,17 +1,28 @@
 let target = document.getElementById("target");
+let img = null;
 
 let melos = {
     name: "melos",
-    src: "images/melos2.png",
+    srcWhenMoveToRight: "images/melos2.png",
+    srcWhenMoveToBottom: "images/melos3.png",
     positionX: 0,
     positionY: 0,
-    moveToRight: (event)=>{
+    latestDirect: 1,
+    moveToRight: (target)=>{
+        if(melos.latestDirect == 0){
+            melos.latestDirect = 1;
+            target.src = melos.srcWhenMoveToRight;
+        }
         melos.positionX += 1;
-        event.target.style.left = `${melos.positionX}px`;
+        target.style.left = `${melos.positionX}px`;
     },
-    moveToBottom: (event)=>{
+    moveToBottom: (target)=>{
+        if(melos.latestDirect == 1){
+            melos.latestDirect = 0;
+            target.src = melos.srcWhenMoveToBottom;
+        }
         melos.positionY += 1;
-        event.target.style.top = `${melos.positionY}px`;
+        target.style.top = `${melos.positionY}px`;
     }
 }
 
@@ -20,28 +31,36 @@ appendMelosImg(target, melos);
 function appendMelosImg(target, melos){
     let img = document.createElement("img");
     img.id = melos.name;
-    img.src = melos.src;
+    img.src = melos.srcWhenMoveToRight;
     img.setAttribute("style", `position:absolute; top:${melos.positionX}px; left:${melos.positionY}px; max-width: 50px;`);
     target.append(img);
 }
 
-document.getElementById("melos").addEventListener("click", function(event){
-    test2(event);
+img = document.getElementById("melos");
+
+addEventListener("keydown", function(event){
+    if(event.key == "ArrowRight") moveToRight(img);
+    if(event.key == "ArrowDown") moveToBottom(img);
 });
 
-function test2(event){
+function moveToBottom(target){
     let counter = 0;
     let i = setInterval(function(){
         counter++;
-        test(event);
-        if(counter >= 32){
+        melos.moveToBottom(img);
+        if(counter >= 16){
             clearInterval(i);
         }
-    }, 32);
+    }, 8);
 }
 
-function test(event){
-    melos.positionX += 1;
-    melos.moveToBottom(event);
-    event.target.style.left = `${melos.positionX}px`;
+function moveToRight(target){
+    let counter = 0;
+    let i = setInterval(function(){
+        counter++;
+        melos.moveToRight(img);
+        if(counter >= 16){
+            clearInterval(i);
+        }
+    }, 8);
 }
